40c9607d5c08f118314d3110e0567b077bb84b4d
Kiran <mailcoding125@gmail.com>
2025-06-25 23:15:54 +0530

    Add Demo React Project

---
+++ my-app/src/First.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function First() {
+  return (
+    <div>
+      <h1>Kiran Morpalle</h1>
+      <p>I'm a Software Developer</p>
+      <img src="./src/assets/developer.webp" alt="developer" />
+    </div>
+  )
+}
+
+export default First
